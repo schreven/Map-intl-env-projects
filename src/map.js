8e40383bc83f6206d5bd06b9ad4c87dee215ad95
Can Altıniğne <canaltinigne@gmail.com>
2018-11-13 16:05:17 +0100

    Map improved

--- src/map.js
+++ src/map.js
@@ -1,9 +1,9 @@
  // Countries to show
 
  const COUNTRIES = [
-    'United States of America',
     'Costa Rica',
     'Ecuador',
+    'United States of America',
     'Brazil',
     'United Kingdom',
     'France',
@@ -18,15 +18,81 @@
  
  // Create variable to hold map element, give initial settings to map
  var map = L.map('map',{  center: [20.0, 0.0], zoom: 3});
- var layer = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
- map.addLayer(layer);      // Adding layer to the map
+
+ var CartoDB_Voyager = new L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
+	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://carto.com/attributions">CARTO</a>',
+	subdomains: 'abcd',
+	maxZoom: 19
+});
+
+map.addLayer(CartoDB_Voyager);      // Adding layer to the map
+
+function onClick(e) {
+    /*popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(map);
+    */
+   // CLICK EVENT
+}
+
+function zoomToFeature(e) {
+    map.fitBounds(e.target.getBounds());
+}
+
+function highlightFeature(e) {
+    var layer = e.target;
+
+    layer.setStyle({
+        weight: 5,
+        color: '#666',
+        dashArray: '',
+        fillOpacity: 0.7
+    });
+
+    if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+        layer.bringToFront();
+    }
+}
+
+function resetHighlight(e) {
+    geojson.resetStyle(e.target);
+}
+
+function onEachFeature(feature, layer) {
+
+    console.log(feature);
+
+    layer.on({
+        mouseover: highlightFeature,
+        mouseout: resetHighlight,
+        click: zoomToFeature
+    });
+}
+
+var geojson;
+// ... our listeners
+
+var myStyle = {
+    "color": "#ff7800",
+    "weight": 5,
+    "opacity": 0.65
+};
+
+map.on('click', onClick);
 
 function filter_countries(data) {
-    if (COUNTRIES.includes(data.properties.sovereignt)) 
+    if (COUNTRIES.includes(data.properties.admin)){
         return true;
+    } 
+
+    return false;
+        
 };
 
  // Add OpenStreetMap tile layer to map element
- $.getJSON('https://d2ad6b4ur7yvpq.cloudfront.net/naturalearth-3.3.0/ne_50m_admin_0_countries.geojson', function(data) {
-    L.geoJson(data, {filter: filter_countries}).addTo(map);
-  });
+ $.getJSON('countries.geojson', function(data) {    
+    geojson = L.geoJson(data, {filter: filter_countries, 
+        style: myStyle,
+        onEachFeature: onEachFeature}).addTo(map);
+});
